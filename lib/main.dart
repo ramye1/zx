e0ff7b5a7139fde28e8edfa3aa0f1999a0e3e45c
ramye1 <crtn2000m@gmail.com>
2026-07-13 13:34:19 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+dart
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:masra_al_dokhail/presentation/pages/canvas_page.dart';
+import 'package:masra_al_dokhail/presentation/providers/app_providers.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ProviderScope(child: MasraAlDokhailApp()));
+}
+
+class MasraAlDokhailApp extends StatelessWidget {
+  const MasraAlDokhailApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'مرسم الدخيل',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const CanvasPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}

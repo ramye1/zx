2550b976f17a14e3804c2e4389c1c2e0aa37c370
ramye1 <crtn2000m@gmail.com>
2026-07-13 13:32:54 +0300

    Create rect_extension.dart

---
+++ lib/core/extensions/rect_extension.dart
@@ -0,0 +1,38 @@
+dart
+import 'package:flutter/material.dart';
+
+extension RectExtension on Rect {
+  bool containsPoint(Offset point) {
+    return left <= point.dx &&
+        point.dx <= right &&
+        top <= point.dy &&
+        point.dy <= bottom;
+  }
+
+  bool intersects(Rect other) {
+    return !(right < other.left ||
+        other.right < left ||
+        bottom < other.top ||
+        other.bottom < top);
+  }
+
+  Rect expandByDistance(double distance) {
+    return Rect.fromLTRB(
+      left - distance,
+      top - distance,
+      right + distance,
+      bottom + distance,
+    );
+  }
+
+  Offset get center => Offset(left + width / 2, top + height / 2);
+
+  Rect translate(Offset offset) {
+    return Rect.fromLTRB(
+      left + offset.dx,
+      top + offset.dy,
+      right + offset.dx,
+      bottom + offset.dy,
+    );
+  }
+}
